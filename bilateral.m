c7e4e4c005fd6f19e7042bb655270ad930866994
TheLastStack <68169657+TheLastStack@users.noreply.github.com>
2021-10-03 15:13:52 +0530

    Added gaussian kernel + bilateral

---
+++ bilateral.m
@@ -0,0 +1,34 @@
+clear all;
+% Parameters % 
+img = imread('Cameraman.png');
+KERNEL_SIZE = 2;
+sp_sig = 1;
+in_sig = 1;
+
+[height, width] = size(img);
+k_end = 2 * KERNEL_SIZE + 1;
+g_sp = zeros(k_end, k_end);
+sp_var = sp_sig ^ 2;
+for i = 1:k_end
+    for j = 1:k_end
+        g_sp(i, j) = exp(- ((i - KERNEL_SIZE - 1)^2 + (j - KERNEL_SIZE - 1)^2) / (2 * sp_var));
+    end
+end
+sp_norm = sum(g_sp, 'all');
+g_sp = g_sp ./ sp_norm;
+
+in_var = in_sig ^ 2;
+final_img = zeros(height, width);
+g_inten = zeros(k_end, k_end);
+for i = 1 + KERNEL_SIZE:height - KERNEL_SIZE
+    for j = 1 + KERNEL_SIZE: width - KERNEL_SIZE
+        for m = -KERNEL_SIZE:KERNEL_SIZE
+            for n = -KERNEL_SIZE:KERNEL_SIZE
+                g_inten(m + KERNEL_SIZE + 1, n + KERNEL_SIZE + 1) = exp(-(double(img(i, j) - img(i + m, j + n))).^2 / (2 * in_var));
+            end
+        end
+        in_norm = sum(g_inten(:));
+        g_inten = g_inten ./ in_norm;
+        final_img(i, j) = double(img(i - KERNEL_SIZE: i+KERNEL_SIZE, j - KERNEL_SIZE:j + KERNEL_SIZE)) .* g_sp .* g_inten;
+    end
+end
